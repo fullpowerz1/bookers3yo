db4e5c3fdffb3b9cf705e843b91243d0453f6393
Taiki Sonoyama <109128636+fullpowerz1@users.noreply.github.com>
2023-02-18 13:58:11 +0000

    tyouun

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -9,6 +11,30 @@ class BooksController < ApplicationController
   end
 
   def new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+    # indexのpath別パターン
+    # redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
